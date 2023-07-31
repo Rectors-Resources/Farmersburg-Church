ee561bf57dbb9ea02dd9fe08972427de6692c70a
Rectors-Resources <brector77777@gmail.com>
2023-07-30 21:02:56 -0400

    maybe

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -4,21 +4,33 @@ export default function Contact() {
   return (
     <form
       onSubmit={(e) => {
-        e.preventDefault()
+        console.log("submitted?");
       }}
       method="post"
       name="contact"
     >
-      <input type="hidden" name="contact" value='contact' />
+      <input type="hidden" name="contact" value="contact" />
       <div>
         <label htmlFor="first-name">First-Name:</label>
-        <input type="text" name='first-name' id="first-name" placeholder="First-Name" required />
+        <input
+          type="text"
+          name="first-name"
+          id="first-name"
+          placeholder="First-Name"
+          required
+        />
         <br />
         <label htmlFor="last-name">Last-Name:</label>
-        <input type="text" id="last-name" name="last-name" placeholder="Last-Name" required />
+        <input
+          type="text"
+          id="last-name"
+          name="last-name"
+          placeholder="Last-Name"
+          required
+        />
         <br />
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" placeholder="E-Mail" name="email"/>
+        <input type="text" id="email" placeholder="E-Mail" name="email" />
       </div>
       <div>
         <textarea
