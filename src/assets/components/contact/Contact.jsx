d0844d0774e8b00bc25abdc2f0d718faa7261b0f
Rectors-Resources <brector77777@gmail.com>
2023-07-30 21:13:09 -0400

    fetch form request

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -1,14 +1,21 @@
 import "./contact.css";
 
 export default function Contact() {
+  const handleSubmit = (event) => {
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .catch((error) => alert(error));
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        console.log("submitted?");
-      }}
-      method="post"
-      name="contact"
-    >
+    <form onSubmit={handleSubmit} name="contact">
       <input type="hidden" name="contact" value="contact" />
       <div>
         <label htmlFor="first-name">First-Name:</label>
