85d7f0e9cdda7f00c958ea06f40f49666a416693
Rectors-Resources <brector77777@gmail.com>
2023-08-03 17:26:02 -0400

    idek if this will work

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -1,27 +1,10 @@
-import { useState } from "react";
 import "./contact.css";
-import axios from "axios";
 
 export default function Contact() {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    comment: "",
-  });
-  const handleOnChange = (event) => {
-    event.persist();
-    setFormData((prev) => ({
-      ...prev,
-      [event.target.id]: event.target.value,
-    }));
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+  
 
   return (
-    <form name="contact" method="post" onSubmit={handleSubmit}>
+    <form method="post" >
       <input type="hidden" name="form-name" value="contact" />
       <div>
         <label htmlFor="firstName">First-Name:</label>
@@ -29,7 +12,6 @@ export default function Contact() {
           type="text"
           name="firstName"
           placeholder="First-Name"
-          onChange={handleOnChange}
           required
         />
         <br />
@@ -38,15 +20,12 @@ export default function Contact() {
           type="text"
           name="lastName"
           placeholder="Last-Name"
-          onChange={handleOnChange}
           required
         />
         <br />
         <label htmlFor="email">Email:</label>
         <input
           type="text"
-          id="email"
-          onChange={handleOnChange}
           placeholder="E-Mail"
           name="email"
         />
@@ -55,7 +34,6 @@ export default function Contact() {
         <textarea
           name="comment"
           placeholder="Submit this form with any questions or requests."
-          onChange={handleOnChange}
           required
         ></textarea>
       </div>
